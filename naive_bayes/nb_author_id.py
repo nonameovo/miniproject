9fa8e112d460d60cf7e8fb82e83bf2db0c074849
Haley Hua <Haleyflower.hua@gmail.com>
2018-03-03 20:47:44 -0600

    change to python3 and build naive bayes model

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -9,7 +9,7 @@
     Sara has label 0
     Chris has label 1
 """
-    
+#Python2    
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -22,11 +22,19 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
+# You can continue with the rest of your code here
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print ("training time:", round(time()-t0, 3), "s")
+
+accuracy = clf.score(features_test, labels_test)
+
+print("Accuracy:", round(accuracy,3))
 
 #########################################################
 
