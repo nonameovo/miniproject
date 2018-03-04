33b64b921147bde8e1bdbac1e4de4a6175fcca1a
Haley Hua <Haleyflower.hua@gmail.com>
2018-03-04 15:29:48 -0600

    mini dataset

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -20,10 +20,39 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+print(type(features_train))
+#features_train =  features_train.astype('int')
+#labels_train = labels_train.astype('int')
 
+
+features_train = features_train[:len(features_train)//100] 
+labels_train = labels_train[:len(labels_train)//100] 
 
 #########################################################
 ### your code goes here ###
+
+for cvalue in [10,100,1000,10000]:
+	from sklearn import svm
+	clf = svm.SVC(kernel='rbf', C=cvalue)
+
+	print(len(features_train))
+	print(len(features_test))
+	print(len(labels_train))
+	print(len(labels_test))
+
+	t0 = time()
+	clf.fit(features_train, labels_train)
+	print ("training time:", round(time()-t0, 3), "s")
+
+	accuracy = clf.score(features_test, labels_test)
+
+	print("Accuracy:", round(accuracy,5))
+
+	#import matplotlib.pyplot as plt
+	#from adspy_shared_utilities import plot_class_regions_for_classifier_subplot
+	#fig, subaxes = plt.subplots(1, 1, figsize=(7, 5))
+	#title = 'Linear SVC, C = {:.3f}'.format(cvalue)
+	#plt.plot(clf, features_train, labels_train, title)
 
 #########################################################
 
