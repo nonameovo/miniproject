c08f81d652734d4fdbb07358acc465f77885b593
Haley Hua <Haleyflower.hua@gmail.com>
2018-04-02 18:29:33 -0500

    modify

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -16,16 +16,31 @@ import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb") )
 
 ### add more features to features_list!
 features_list = ["poi", "salary"]
 
 data = featureFormat(data_dict, features_list)
+sort_keys = '../tools/python2_lesson14_keys.pkl'
 labels, features = targetFeatureSplit(data)
 
 
 
 ### your code goes here 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import cross_validation
+
+features_train,features_test,labels_train,labels_test = cross_validation.train_test_split(features,labels,test_size=0.3,
+                                                                                          random_state=42)
+clf = DecisionTreeClassifier()
+clf.fit(features_train,labels_train)
+clf.score(features_test,labels_test)
+
+# How many POIs are in the test set for your POI identifier?
+pred = clf.predict(features_test)
+sum(pred)
 
 
+print("total people in test set",len(labels_test))
+print("total POI in test set",len([e for e in labels_test if e == 1.0]))
